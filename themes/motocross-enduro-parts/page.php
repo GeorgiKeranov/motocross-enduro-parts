9a0676bbcc8bc9954cd8f5b1576f3958e2c8b7bb
Georgi <georgy.keranov@gmail.com>
2020-11-16 15:43:51 +0200

    Add desktop modifications to styles in the shop page

--- themes/motocross-enduro-parts/page.php
+++ themes/motocross-enduro-parts/page.php
@@ -4,10 +4,24 @@ the_post();
 get_header();
 ?>
 
-<div class="section-default">
+<div class="section-intro-alt">
 	<div class="shell">
 		<h1><?php the_title() ?></h1>
 
+		<ul class="section__links">
+			<li>
+				<p><a href="<?php echo home_url('/') ?>"><i class="ico-home"></i> Начало</a></p>
+			</li>
+			
+			<li>
+				<p><?php the_title() ?></p>
+			</li>
+		</ul><!-- /.section__links -->
+	</div><!-- /.shell -->
+</div><!-- /.section-intro-alt -->
+
+<div class="section-default">
+	<div class="shell">
 		<?php the_content() ?>
 	</div><!-- /.shell -->
 </div><!-- /.section-default -->
