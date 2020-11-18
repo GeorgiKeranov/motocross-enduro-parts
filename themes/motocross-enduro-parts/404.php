b213c13c00b104eaf06e176b0d1eabd203516e64
Georgi <georgy.keranov@gmail.com>
2020-11-18 23:32:17 +0200

    Add 404 styles for desktop and mobile

--- themes/motocross-enduro-parts/404.php
+++ themes/motocross-enduro-parts/404.php
@@ -1,8 +1,14 @@
+<?php get_header(); ?>
+
+<div class="section-404">
+	<div class="shell">
+		<div class="section__inner">
+			<h1>Грешка 404</h1>
+
+			<p>Моля, проверете URL адреса за правилно изписване. Ако имате проблем с намирането на страницата пробвайте да отворите <a href="<?php echo home_url(); ?>">началната страница</a>.</p>
+		</div><!-- /.section__inner -->
+	</div><!-- /.shell -->
+</div><!-- /.section-404 -->
+
 <?php
-get_header();
-
-printf( __( '<p>Please check the URL for proper spelling and capitalization.<br />If you\'re having trouble locating a destination, try visiting the <a href="%1$s">home page</a>.</p>', 'crb' ), home_url( '/' ) );
-
-get_sidebar();
-
 get_footer();
