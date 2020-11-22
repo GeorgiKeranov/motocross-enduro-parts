502d677908ef4821d8f5f798663c7cf1dfc4308b
Georgi <georgy.keranov@gmail.com>
2020-11-22 21:08:30 +0200

    Add new js file for loading selects of the new metabox dynamically

--- themes/motocross-enduro-parts/compatible-motorcycles/load-values-in-selects-dynamically.js
+++ themes/motocross-enduro-parts/compatible-motorcycles/load-values-in-selects-dynamically.js
@@ -1,3 +1,14 @@
 (function($) {
+	if (typeof jsonMotorcyclesTypes === undefined ) {
+		return;
+	}
 
+	let $compatibleMotorcycles = $('.compatible-motorcycles');
+
+	$compatibleMotorcycles.on('change', '.compatible-motorcycle-make', function() {
+		let $this = $(this);
+		let selectedMake = $this.val();
+
+
+	});
 })(jQuery);
