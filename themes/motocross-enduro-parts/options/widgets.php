6df09bb1a8c2f74b9abe00f2bf89945e7ccf4bf9
Georgi <georgy.keranov@gmail.com>
2020-11-24 00:23:09 +0200

    Add the custom widget option to make it as select

--- themes/motocross-enduro-parts/options/widgets.php
+++ themes/motocross-enduro-parts/options/widgets.php
@@ -16,7 +16,16 @@ class Product_Categories_Widget extends WP_Widget {
 	}
 
 	public function widget( $args, $instance ) {
-		$title = esc_html( $instance['title'] );
+		$title = '';
+		$is_select_menu = false;
+
+		if ( isset( $instance['title'] ) ) {
+			$title = esc_html( $instance['title'] );
+		}
+
+		if ( isset( $instance['is_select_menu'] ) ) {
+			$is_select_menu = $instance['is_select_menu'];
+		}
 
 		$product_categories = get_terms( array(
 			'taxonomy' => 'product_cat',
@@ -34,13 +43,23 @@ class Product_Categories_Widget extends WP_Widget {
 			echo $args['before_title'] . $title . $args['after_title'];
 		} ?>
 
-		<ul class="product-categories">
-			<?php foreach ( $product_categories as $category ) : ?>
-				<li class="porduct__category">
-					<a href="<?php echo $category->term_id ?>"><?php echo esc_html( $category->name ) ?></a>
-				</li>
-			<?php endforeach; ?>
-		</ul>
+		<?php if ( $is_select_menu == 'on' ) : ?>
+			<select name="product-categories" class="product-categories-select">
+				<option value="" default>Избиране на категория</option>
+
+				<?php foreach ( $product_categories as $category ) : ?>
+					<option value="<?php echo $category->term_id ?>"><?php echo esc_html( $category->name ) ?></option>					
+				<?php endforeach; ?>
+			</select>
+		<?php else : ?>
+			<ul class="product-categories">
+				<?php foreach ( $product_categories as $category ) : ?>
+					<li class="porduct__category">
+						<a href="<?php echo $category->term_id ?>"><?php echo esc_html( $category->name ) ?></a>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+		<?php endif; ?>
 
 		<?php
 		echo $args['after_widget'];
@@ -48,14 +67,25 @@ class Product_Categories_Widget extends WP_Widget {
 
 	public function form( $instance ) {
 		$title = '';
+		$is_select_menu = false;
 
-		if ( isset( $instance[ 'title' ] ) ) {
-			$title = $instance[ 'title' ];
-		} ?>
+		if ( isset( $instance['title'] ) ) {
+			$title = $instance['title'];
+		}
+
+		if ( isset( $instance['is_select_menu'] ) ) {
+			$is_select_menu = $instance['is_select_menu'];
+		}
+		?>
 		
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Заглавие:', 'crb' ); ?></label> 
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+
+		<p>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'is_select_menu' ); ?>" name="<?php echo $this->get_field_name( 'is_select_menu' ); ?>" type="checkbox"<?php echo $is_select_menu == 'on' ? ' checked' : '' ?>/>
+			<label for="<?php echo $this->get_field_id( 'is_select_menu' ); ?>">Показване като падащо меню</label>
 		</p>
 		
 		<?php 
