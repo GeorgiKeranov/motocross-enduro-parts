ec4108d642f191b2c35fb34361634d58d50ea89b
Georgi <georgy.keranov@gmail.com>
2020-12-17 20:28:10 +0200

    Add custom meta to the product's content

---
+++ themes/motocross-enduro-parts/woocommerce/single-product/meta.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Single Product Meta
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/meta.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     3.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+global $product;
+?>
+<div class="product_meta">
+
+	<?php do_action( 'woocommerce_product_meta_start' ); ?>
+
+	<?php if ( wc_product_sku_enabled() && ( $product->get_sku() || $product->is_type( 'variable' ) ) ) : ?>
+
+		<span class="sku_wrapper"><?php esc_html_e( 'SKU:', 'woocommerce' ); ?> <span class="sku"><?php echo ( $sku = $product->get_sku() ) ? $sku : esc_html__( 'N/A', 'woocommerce' ); ?></span></span>
+
+	<?php endif;
+
+	$shop_page_permalink = '';
+	$shop_page_id = wc_get_page_id( 'shop' );
+	if ( !empty( $shop_page_id ) ) {
+		$shop_page_permalink = get_permalink( $shop_page_id );
+	}
+
+	$categories = get_the_terms( $product->get_id(), 'product_cat' );
+	if ( !empty( $categories ) ) :
+		$categories_count = count( $categories ); ?>
+
+		<span class="posted_in">
+			<?php
+			if ( $categories_count === 1 ) {
+				echo __( 'Category', 'woocommerce' ) . ': ';
+			} else {
+				echo __( 'Categories', 'woocommerce' ) . ': ';
+			} ?>
+
+			<?php foreach ( $categories as $index => $category ) : ?>
+				<a href="<?php echo $shop_page_permalink . '?category=' . $category->term_id ?>" rel="tag"><?php echo esc_html( $category->name ) ?></a><?php
+
+				if ( ($index + 1) < $categories_count ) {
+					echo ',';
+				}
+			endforeach; ?>
+		</span>
+	<?php endif;
+
+	echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
+
+	<?php do_action( 'woocommerce_product_meta_end' ); ?>
+
+</div>
