ccd5b8a40e7bbb857cd11a94d3645194d3749e08
Georgi <georgy.keranov@gmail.com>
2021-03-10 22:59:14 +0200

    Add back end functionality for the new admin page

--- themes/motocross-enduro-parts/options/motorcycles.php
+++ themes/motocross-enduro-parts/options/motorcycles.php
@@ -56,10 +56,27 @@ function crb_add_motorcycle_pages() {
 		__( 'Attach Parts To Motorcycles', 'crb' ),
 		'manage_options',
 		'attach_parts_to_motorcycles',
-		'crb_admin_page_attarch_parts_to_motorcycles'
+		'crb_attach_parts_to_motorcycles'
 	);
 }
 
-function crb_admin_page_attarch_parts_to_motorcycles() {
+function crb_attach_parts_to_motorcycles() {
+	if ( isset( $_POST ) ) {
+		crb_save_attached_parts_to_motorcycles( $_POST );
+	}
+
 	crb_render_fragment( 'admin/attach-motorcycle-parts-to-motorcycle' );
+}
+
+function crb_save_attached_parts_to_motorcycles( $params ) {
+	if ( empty( $params['selected_parts'] ) || empty( $params['motorcycle'] ) ) {
+		return;
+	}
+
+	$selected_parts = $params['selected_parts'];
+	$motorcycle = $params['motorcycle'];
+
+	foreach ( $selected_parts as $selected_part_id ) {
+		carbon_set_post_meta( $selected_part_id, 'crb_part_disassembled_from_motorcycle', $motorcycle );
+	}
 }
