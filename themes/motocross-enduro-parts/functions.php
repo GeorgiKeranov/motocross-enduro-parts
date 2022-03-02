1b40fe8eedc829edb59a2db8da63ad7d5653cb7f
Georgi <georgy.keranov@gmail.com>
2022-03-02 12:08:15 +0200

    Stop email notifications about automatic updates

--- themes/motocross-enduro-parts/functions.php
+++ themes/motocross-enduro-parts/functions.php
@@ -188,3 +188,21 @@ function crb_remove_admin_login_header() {
  * Stop the email notifications for automatic plugin updates
  */
 add_filter( 'auto_plugin_update_send_email', '__return_false' );
+
+
+/**
+ * Stop the email notifications for automatic theme updates
+ */
+add_filter( 'auto_theme_update_send_email', '__return_false' );
+
+/**
+ * Stop the email notifications for automatic core updates
+ */
+add_filter( 'auto_core_update_send_email', 'wpb_stop_auto_update_emails', 10, 4 );
+function wpb_stop_update_emails( $send, $type, $core_update, $result ) {
+	if ( ! empty( $type ) && $type == 'success' ) {
+		return false;
+	}
+	
+	return true;
+}
